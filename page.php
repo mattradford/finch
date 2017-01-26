36ed889b40bb6d7c9a483d17aa0e5a559d6ad268
Matt Radford <webform@mattrad.co.uk>
2017-01-26 21:45:11 +0000

    Ability to hide page title

--- page.php
+++ page.php
@@ -1,6 +1,8 @@
 <div class="page__wrap <?php if (has_post_thumbnail()) { echo ' page__image'; } ?>">
         <?php while (have_posts()) : the_post(); ?>
-            <div class="page__title"><h1><?php echo roots_title(); ?></h1></div>
+            <?php if ( !get_field( 'hide_page_title' ) ) { ?>
+                <div class="page__title"><h1><?php echo roots_title(); ?></h1></div>
+            <?php } ?>
             <?php get_template_part('templates/content', 'page'); ?>
             <?php get_template_part('templates/sidebar'); ?>
         <?php endwhile; ?>
--- template-page-builder.php
+++ template-page-builder.php
@@ -5,7 +5,7 @@ Template Name: Page Builder
 ?>
 <div class="page__wrap <?php if (has_post_thumbnail()) { echo ' page__image'; } ?>">
 <?php while (have_posts()) : the_post(); ?>
-    <?php if ( !is_front_page() ) { ?>
+    <?php if ( !is_front_page() || !get_field( 'hide_page_title' ) ) { ?>
         <div class="page__title"><h1><?php echo roots_title(); ?></h1></div>
     <?php } ?>
     <?php get_template_part('templates/content', 'page-builder'); ?>
